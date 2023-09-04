8be23721f7b4334c211d9229b7f4214fa9276928
tanpeihao <1939432358@qq.com>
2023-09-04 23:22:29 +0800

    test deploy

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react';
 
 export type ButtonProps = {
-  /**
-   * 自定义内容
+  /** 自定义内容
+   * @default -
    */
   children?: React.ReactNode;
+  /** 是否禁用
+   * @default false
+   * */
+  disabled?: boolean;
   /** 按钮形状
    *  @default 'default'
    * */
@@ -13,8 +17,7 @@ export type ButtonProps = {
    * @default 'middle'
    * */
   size?: 'large' | 'middle' | 'small';
-  /**
-   * 按钮类型
+  /** 按钮类型
    * @default 'default'
    */
   theme?: 'primary' | 'default' | 'dashed' | 'text' | 'link';
