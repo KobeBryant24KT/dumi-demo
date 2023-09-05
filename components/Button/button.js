8504b87ff4d0842e6aeb97e9d6c19d0a300b905c
KobeBryant24KT <KobeBryant24KT@users.noreply.github.com>
2023-09-05 14:40:31 +0000

    deploy: 0780fa1bed2d42dfd48b94a7a7aafb7c3216daa9

--- components/Button/button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-var Button = function Button(props) {
-  var children = props.children;
-  return /*#__PURE__*/React.createElement("button", null, children);
-};
-export default Button;
